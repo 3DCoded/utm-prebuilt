6cd1edde36d21692d1e59fca44c9f86c456d60bc
Sandra Mattar <69043333+smattar2020@users.noreply.github.com>
2022-06-22 08:51:18 -0400

    start templates

---
+++ generator.py
@@ -0,0 +1,22 @@
+import json
+
+from jinja2 import Environment, FileSystemLoader, Template
+from yaml import load
+
+with open('data.json') as file:
+    items = json.load(file)
+
+
+def render_template(filename, **kwargs):
+    with open(f'templates/{filename}') as file:
+        template = Environment(loader=FileSystemLoader(
+            searchpath='./')).from_string(file.read())
+        return template.render(**kwargs)
+
+
+def build_template(filename, dest, **kwargs):
+    with open(f'built/{dest}', 'w') as file:
+        file.write(render_template(filename, **kwargs))
+
+
+build_template('index.html', 'index.html')
